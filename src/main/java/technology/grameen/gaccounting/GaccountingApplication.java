0837627b5badef6e66ca0cf217eed225cf5959cd
mehearaz <mu.himel@gmail.com>
2021-05-17 08:00:44 +0600

    'update'

--- src/main/java/technology/grameen/gaccounting/GaccountingApplication.java
+++ src/main/java/technology/grameen/gaccounting/GaccountingApplication.java
@@ -2,31 +2,11 @@ package technology.grameen.gaccounting;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class GaccountingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GaccountingApplication.class, args);
-	}
-
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-
-		String liveUrl="http://103.26.136.30";
-		String devUrl="http://localhost:8081";
-
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-
-				registry.addMapping("/api/v1/**")
-						.allowedMethods("OPTIONS","GET","POST","PUT","DELETE")
-						.allowedOrigins(devUrl);
-			}
-		};
 	}
 }
---
+++ src/main/java/technology/grameen/gaccounting/configs/WebOriginConfig.java
@@ -0,0 +1,29 @@
+package technology.grameen.gaccounting.configs;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+@PropertySource("classpath:application.properties")
+public class WebOriginConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private Environment env;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+
+        String origins = env.getProperty("origins");
+
+        registry.addMapping("/api/v1/**")
+						.allowedMethods("OPTIONS","GET","POST","PUT","DELETE")
+						.allowedOrigins(origins.split(","));
+    }
+}
