e8d8761a4ef4853a331c5d4301d0e9bb0b3e6bde
muhimel <mu.himel@gmail.com>
2021-05-24 16:13:16 +0600

    'some-fix'

--- src/main/java/technology/grameen/gaccounting/accounting/repositories/CaRepository.java
+++ src/main/java/technology/grameen/gaccounting/accounting/repositories/CaRepository.java
@@ -21,17 +21,19 @@ public interface CaRepository extends JpaRepository<ChartAccount,Long> {
     List<ChartAccountList> findAllChartAccounts();
 
     @Query(value = "SELECT cas.id, cat.name as typeName,cat.code as ctCode,cas.title,cas.code as caCode, " +
-            "cas.is_ledger as IsLedger,cal.contact_address as contactAddress,cal.contact_name as contactName, " +
+            "parentCas.title as parent, parentCas.id as parentId, cas.is_ledger as IsLedger,cal.contact_address as contactAddress,cal.contact_name as contactName, " +
             "cal.contact_email as contactEmail,cal.contact_phone as contactPhone FROM CA_TYPES cat " +
             "JOIN CHART_ACCOUNTS cas ON cas.CHART_ACCOUNT_TYPE_ID = cat.id " +
             "LEFT JOIN CA_LEDGERS cal ON cal.CHART_ACCOUNT_ID = cas.id " +
+            "LEFT JOIN CHART_ACCOUNTS parentCas ON cas.PARENT_ID = parentCas.id " +
             " WHERE cas.is_ledger=1 " +
             "ORDER BY cat.code ASC, cas.code ASC",nativeQuery = true)
     List<LedgerAccountList> findAllLedgerAccounts();
 
-    @Query(value = "SELECT cas.id, cat.name as typeName,cat.code as ctCode,cas.title,cas.code as caCode, " +
-            "cas.is_ledger as IsLedger FROM CA_TYPES cat " +
+    @Query(value = "SELECT cas.id, cat.id as caTypeId, cat.name as typeName,cat.code as ctCode,cas.title,cas.code as caCode, " +
+            "parentCas.title as parent, parentCas.id as parentId, cas.is_ledger as IsLedger FROM CA_TYPES cat " +
             "JOIN CHART_ACCOUNTS cas ON cas.CHART_ACCOUNT_TYPE_ID = cat.id " +
+            "LEFT JOIN CHART_ACCOUNTS parentCas ON cas.PARENT_ID = parentCas.id " +
             " WHERE cas.is_ledger=0 " +
             "ORDER BY cat.code ASC, cas.code ASC",nativeQuery = true)
     List<ChartAccountList> findAllGroupAccounts();
--- src/main/java/technology/grameen/gaccounting/projection/authserver/ChartAccountList.java
+++ src/main/java/technology/grameen/gaccounting/projection/authserver/ChartAccountList.java
@@ -2,6 +2,7 @@ package technology.grameen.gaccounting.projection.authserver;
 
 public interface ChartAccountList {
     Long getId();
+    Integer getCaTypeId();
     String getTypeName();
     String getCtCode();
     String getTitle();
