eaf95f9c2a6bf8f93575b6106cf4440664e3d67b
muhimel <mu.himel@gmail.com>
2021-06-02 11:50:52 +0600

    'some-update'

---
+++ src/main/java/technology/grameen/gaccounting/projection/VoucherDetail.java
@@ -0,0 +1,46 @@
+package technology.grameen.gaccounting.projection;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Set;
+
+public interface VoucherDetail {
+
+    interface VoucherType{
+        Integer getId();
+        String getAlias();
+    }
+
+    interface ChartAccount{
+        Long getId();
+        String getTitle();
+    }
+
+    interface Transaction{
+        Long getId();
+        BigDecimal getAmount();
+        String getTransactionType();
+        String getNarration();
+        ChartAccount getChartAccount();
+        String getRealm();
+        Integer getOfficeId();
+        Short getOfficeTypeId();
+    }
+
+    Long getId();
+    VoucherType getVoucherType();
+    String getVoucherNo();
+    BigDecimal getTotalDebitAmount();
+    BigDecimal getTotalCreditAmount();
+    String getNote();
+    Boolean getStatus();
+    Integer getOfficeId();
+    Short getOfficeTypeId();
+    String getRealm();
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    LocalDateTime getVoucherDate();
+    Set<Transaction> getTransactions();
+
+}
--- src/main/java/technology/grameen/gaccounting/services/voucher/VoucherService.java
+++ src/main/java/technology/grameen/gaccounting/services/voucher/VoucherService.java
@@ -4,7 +4,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import technology.grameen.gaccounting.accounting.entity.Voucher;
 import technology.grameen.gaccounting.exceptions.CustomException;
+import technology.grameen.gaccounting.projection.VoucherDetail;
 import technology.grameen.gaccounting.projection.VoucherList;
+
+import java.util.Optional;
 
 public interface VoucherService {
 
@@ -12,4 +15,6 @@ public interface VoucherService {
     Page<VoucherList> getVouchers(Pageable pageable);
 
     Voucher addVoucher(Voucher voucher) throws CustomException;
+
+    Optional<VoucherDetail> getVoucherDetail(Long id);
 }
